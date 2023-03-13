2d0992106fea186845b6e0d019e16357f4689f30
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-03-13 17:31:42 -0400

    add skills to json files

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Header from './components/Header'
 import './App.css'
 import Bio from './components/Bio'
 import Projects from './components/Projects'
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Bio/>
       <Projects/>
-      <Header/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer (props) {
+    return <>
+    Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </>
+}
