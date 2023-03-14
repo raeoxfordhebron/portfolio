2a1e85c0eb097186cf261e4603b0330485e2f7f7
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-03-13 20:27:33 -0400

    moved Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
+import { FooterContainer } from "../styled-components/Footer";
+
 export default function Footer (props) {
     return <>
+    <FooterContainer>
     Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </FooterContainer>
     </>
 }
